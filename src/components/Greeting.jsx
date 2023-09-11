139801b1863e20b55aa6af5d17eb4410b8d82426
user <user@user.com>
2023-09-12 00:08:04 +0200

    Personalized Content

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -5,14 +5,14 @@ export default function Greeting({messages}) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
   
-  const [greeting, setGreeting] = useState(messages[0]);
+  const [greeting, setGreeting] = useState(randomMessage);
 
   return (
     <div> 
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      <h3>{greeting}! Welcome to my corner of the Internet!</h3>
+      {/* <button onClick={() => setGreeting(randomMessage())}>
         New Greeting
-      </button>
+      </button> */}
     </div>
   );
 }
---
+++ src/pages/projects/project-1.md
@@ -0,0 +1,10 @@
+---
+layout: ../../layouts/WorkLayout.astro
+title: "Test Project 1"
+description: "Project summary that can span a few lines"
+image:
+  url: "https://docs.astro.build/assets/full-logo-dark.png"
+  alt: "The full Astro logo."
+---
+
+This is a test project
